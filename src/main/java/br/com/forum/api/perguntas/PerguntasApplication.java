0700ad5a29cbdcf8f4887ec3c182b55c767b3c28
Washington <wlanjos@hotmail.com>
2020-12-05 09:52:06 -0300

    Init project

---
+++ src/main/java/br/com/forum/api/perguntas/PerguntasApplication.java
@@ -0,0 +1,13 @@
+package br.com.forum.api.perguntas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerguntasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerguntasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/forum/api/perguntas/PerguntasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.forum.api.perguntas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerguntasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
